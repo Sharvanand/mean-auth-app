bb2c88d6b239bcd185ec9a559ec0ea4e7b2bf563
Sharvanand <thirusharwan@gmail.com>
2017-12-10 19:47:46 -0500

    Database created

--- app.js
+++ app.js
@@ -5,17 +5,28 @@ const bodyParser  = require('body-parser');
 const mongoose    = require('mongoose');
 const passport    = require('passport');
 const users       = require('./routes/users');
+const config      = require('./config/database');
 
 
 const port        = 3000;
 
 const app         = express();
 
-// middlewares..
+mongoose.connect(config.database, (err) =>{
+    if(err) throw err;
+    console.log("Connected to "+ config.database);
+});
+
+// MIDDLEWARES....
 app.use(cors());
 app.use(bodyParser.json( ));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/users',users);
+
+//SET STATIC FOLDERS..
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.get('/', (req, res) =>{
     res.send("Hello World");
